cc28243f75add35ff212e9df7d83247052fc826c
Lalatendu Bal <lalatendu96@gmail.com>
2025-04-06 11:11:47 +0200

    check for submission

--- data.py
+++ data.py
@@ -1,7 +1,6 @@
-import pandas as pd
-from os.path import join
-
-from sklearn.impute import KNNImputer
+import os
+import csv
+from datetime import datetime
 import pandas as pd
 import numpy as np
 
@@ -12,12 +11,12 @@ class DataLoader:
     def load_data(self, country: str):
         date_format = "%Y-%m-%d %H:%M:%S"
 
-        consumptions_path = join(self.path, "historical_metering_data_" + country + ".csv")
+        consumptions_path = os.path.join(self.path, "historical_metering_data_" + country + ".csv")
 
-        features_path = join(self.path, "spv_ec00_forecasts_es_it.xlsx")
-        example_solution_path = join(self.path, "example_set_" + country + ".csv")
+        features_path = os.path.join(self.path, "spv_ec00_forecasts_es_it.xlsx")
+        example_solution_path = os.path.join(self.path, "example_set_" + country + ".csv")
 
-        rollout_path = join(self.path, "rollout_data_" + country + ".csv")
+        rollout_path = os.path.join(self.path, "rollout_data_" + country + ".csv")
 
         rollout = pd.read_csv(rollout_path, index_col=0, parse_dates=True, date_format=date_format)
     
@@ -36,10 +35,6 @@ class DataLoader:
         )
 
         features = pd.merge(features, rollout, on="DateTime", how="left")
-        
-        #drop all indices with NanValue in the consumption and then drop the same indices in the features
-
-
 
         example_solution = pd.read_csv(
             example_solution_path,
@@ -55,7 +50,7 @@ class DataLoader:
 
         date_format = "%Y-%m-%d %H:%M:%S"
 
-        consumptions_path = join(self.path, "historical_metering_data_" + country + ".csv")
+        consumptions_path = os.path.join(self.path, "historical_metering_data_" + country + ".csv")
 
         if split_date == "2024-08-01 00:00:00":
             example_set_path = f"datasets2025\example_set_{country}.csv"
@@ -66,16 +61,9 @@ class DataLoader:
                 date_format=date_format,
             )
 
-        
-        
+        features_path = os.path.join(self.path, "spv_ec00_forecasts_es_it.xlsx")
 
-
-
-
-        features_path = join(self.path, "spv_ec00_forecasts_es_it.xlsx")
-        example_solution_path = join(self.path, "example_set_" + country + ".csv")
-
-        rollout_path = join(self.path, "rollout_data_" + country + ".csv")
+        rollout_path = os.path.join(self.path, "rollout_data_" + country + ".csv")
         rollout = pd.read_csv(rollout_path, index_col=0, parse_dates=True, date_format=date_format)
         rollout = rollout.reset_index()
 
@@ -88,8 +76,6 @@ class DataLoader:
 
             consumptions = pd.concat([consumptions, example_solution])
 
-
-
         features = pd.read_excel(
             features_path,
             sheet_name=country,
@@ -100,11 +86,7 @@ class DataLoader:
 
         features = features.reset_index()
         features = features.rename(columns={"index": "DATETIME"})
-
-
-
         features = pd.merge(features, rollout, on="DATETIME", how="left")
-
 
         # Convert split_date to a datetime object if it's not already one
         split_date = pd.to_datetime(split_date, format=date_format)
@@ -122,103 +104,6 @@ class DataLoader:
         test_features = features[features.index >= split_date]
 
         return train_consumptions, test_consumptions, train_features, test_features
-
-
-
-class SimpleEncoding:
-    """
-    This class is an example of dataset encoding.
-
-    """
-
-    def __init__(
-        self,
-        consumption: pd.Series,
-        features: pd.Series,
-        end_training,
-        start_forecast,
-        end_forecast,
-    ):
-        self.consumption_mask = ~consumption.isna()
-        self.consumption = consumption[self.consumption_mask]
-        self.features = features
-        self.end_training = end_training
-        self.start_forecast = start_forecast
-        self.end_forecast = end_forecast
-
-    def meta_encoding(self):
-        """
-        This function returns the feature, split between past (for training) and future (for forecasting)),
-        as well as the consumption, without missing values.
-        :return: three numpy arrays
-
-        """
-        features_past = self.features[: self.end_training].values.reshape(-1, 1)
-        features_future = self.features[
-            self.start_forecast : self.end_forecast
-        ].values.reshape(-1, 1)
-
-        features_past = features_past[self.consumption_mask]
-        return features_past, features_future, self.consumption
-
-
-class ImputationEncoding:
-    def __init__(self, consumption: pd.DataFrame, features: pd.DataFrame, start_training, end_training, start_forecast, end_forecast, method):
-        self.consumption = consumption
-        self.features = features
-        self.start_training = start_training
-        self.end_training = end_training
-        self.start_forecast = start_forecast
-        self.end_forecast = end_forecast
-        self.consumption_mask = ~consumption.isna()
-        self.method = method
-
-    def meta_encoding(self):
-
-
-        if self.method == KNNImputer:
-            # Impute missing values in the features using KNNImputer
-            imputer = KNNImputer(n_neighbors=5, weights="uniform")
-            self.features = pd.DataFrame(imputer.fit_transform(self.features), columns=self.features.columns, index=self.features.index)
-            self.consumption = pd.Series(imputer.fit_transform(self.consumption.values.reshape(-1, 1)).flatten(), index=self.consumption.index)
-        else:
-            for feature in self.features.columns:
-
-                if self.method == "ffill_bbfill":
-                    # Impute missing values in the feature column using forward fill
-                    self.features[feature] = self.features[feature].ffill()
-                    self.consumption = self.consumption.ffill()
-
-                    self.features[feature] = self.features[feature].bfill()
-                    self.consumption = self.consumption.bfill()
-
-                    if self.consumption.isna().any():
-                        #imput remaining missing values with mean
-                        self.consumption = self.consumption.fillna(self.consumption.mean())
-                        self.features[feature] = self.features[feature].fillna(self.features[feature].mean())
-                    
-                elif self.method == "bfill":
-                    # Impute missing values in the feature column using backward fill
-                    self.features[feature] = self.features[feature].bfill()
-                    self.consumption = self.consumption.bfill()
-                elif self.method == "mean":
-                    # Impute missing values in the feature column using mean
-                    self.features[feature] = self.features[feature].fillna(self.features[feature].mean())
-                    self.consumption = self.consumption.fillna(self.consumption.mean())
-
-
-                    
-
-            
-        #get the features past and future
-        features_past = self.features[self.start_training : self.end_training]
-
-        features_future = self.features[self.start_forecast : self.end_forecast]
-
-        consumption_past = self.consumption[self.start_training : self.end_training]
-        consumption_future = self.consumption[self.start_forecast : self.end_forecast]
-
-        return features_past, features_future, consumption_past, consumption_future
 
 
 class FeatureEncoding:
@@ -288,12 +173,6 @@ class FeatureEncoding:
 
 
 
-
-        
-
-import os
-import csv
-from datetime import datetime
 
 def log_results(model_name, country, error, test_start, test_end, logging_folder, feature_sets, global_model, encoding):
     log_file = os.path.join(logging_folder,country, f"{model_name}_results.csv")
--- load_forecasting.py
+++ load_forecasting.py
@@ -2,16 +2,13 @@ import pandas as pd
 import numpy as np
 from os.path import join
 import os
-import csv
-from sklearn.model_selection import GridSearchCV, PredefinedSplit, TimeSeriesSplit
-from xgboost import XGBRegressor
 # depending on your IDE, you might need to add datathon_eth. in front of data
-from data import DataLoader, SimpleEncoding, log_results, ImputationEncoding, FeatureEncoding
+from data import DataLoader, log_results, FeatureEncoding
 # depending on your IDE, you might need to add datathon_eth. in front of forecast_models
-from forecast_models import SimpleModel, ARIMAModel, GaussianProcessModel, XGBoostModel, GAMModel, CatBoostModel, MultiTimeSeriesForecaster, LightGBMModel
+from forecast_models import LightGBMModel
 import warnings
 warnings.filterwarnings("ignore")
-def main(zone: str, encoding_name: str, model_name: str, train_test: bool, split_date :str , feature_sets:str, global_model: bool, inputation_method:str = "ffill", start_date:str = None):
+def main(zone: str, encoding_name: str, model_name: str, train_test: bool, split_date :str , feature_sets:str, start_date:str = None):
     """
 
     Train and evaluate the models for IT and ES
@@ -30,18 +27,10 @@ def main(zone: str, encoding_name: str, model_name: str, train_test: bool, split
 
     if train_test == True:
         print(f"Train_testing will be done with {split_date} ")
-        training_set, test_set, train_features, test_features = loader.load_data_train_test(country = country, 
-                                                                                                           split_date=split_date
-                                                                                                           )
+        training_set, test_set, train_features, test_features = loader.load_data_train_test(country = country, split_date=split_date)
         
         #join train_features and test_features
         features_train_and_test = pd.concat([train_features,test_features])
-
-        print(features_train_and_test)
-
-        #calculate a lot of features for the training set
-
-
 
     else:
         training_set, features, test_set = loader.load_data(zone)
@@ -68,172 +57,42 @@ def main(zone: str, encoding_name: str, model_name: str, train_test: bool, split
 
     forecast = pd.DataFrame(columns=training_set.columns, index=range_forecast)
 
+    print("We are using Models for each customer")
+    print("------------------------------------------------------")
+    for costumer in training_set.columns.values:
 
-    if global_model:
-        print("We are using a global model")
-        print("------------------------------------------------------")
-
+        print(f"Customer: {costumer}")
+        consumption = training_set.loc[:, costumer]
         if train_test:
-            pass
 
-    else:
-        print("We are using Models for each customer")
-        print("------------------------------------------------------")
-        for costumer in training_set.columns.values:
+            feature_dummy =  features_train_and_test[feature_sets].loc[start_training:]
+            if encoding_name == "calculate_custom_features":
+                feature_dummy =  features_train_and_test.loc[start_training:]
+        else:
+            feature_dummy = features[feature_sets].loc[start_training:]
 
-            print(f"Customer: {costumer}")
-            consumption = training_set.loc[:, costumer]
-            if train_test:
 
-                feature_dummy =  features_train_and_test[feature_sets].loc[start_training:]
-                if encoding_name == "calculate_custom_features":
-                    feature_dummy =  features_train_and_test.loc[start_training:]
-            else:
-                feature_dummy = features[feature_sets].loc[start_training:]
+        if encoding_name=="calculate_custom_features":
 
-            
-            if encoding_name == "baseline_encoding":
-
-                encoding = SimpleEncoding(
-                    consumption, feature_dummy, end_training, start_forecast, end_forecast
-                )
-
-                feature_past, feature_future, consumption_clean = (
-                encoding.meta_encoding()
+            encoding = FeatureEncoding(
+                consumption, feature_dummy, start_training, end_training, start_forecast, end_forecast, customer=costumer
             )
-            elif encoding_name == "custom_encoding":
-                encoding = ImputationEncoding(
-                    consumption, feature_dummy, start_training, end_training, start_forecast, end_forecast, method=inputation_method
-                )
-                feature_past, feature_future, consumption_clean, consumption_test= (
-                    encoding.meta_encoding()
-                )
 
-                pass
-        
-            elif encoding_name=="calculate_custom_features":
+            feature_past, feature_future, consumption_clean, consumption_test = encoding.calculate_features()
 
-                encoding = FeatureEncoding(
-                    consumption, feature_dummy, start_training, end_training, start_forecast, end_forecast, customer=costumer
-                )
+        else:
+            print("NO Encoding specified sofar")
+            raise ValueError("No encoding specified")
 
-                feature_past, feature_future, consumption_clean, consumption_test = encoding.calculate_features()
-
-            else:
-                print("NO Encoding specified sofar")
-                raise ValueError("No encoding specified")
-
-            if model_name == "linear_regression":
-                model = SimpleModel()
-                model.train(feature_past, consumption_clean)
-
-                output = model.predict(feature_future)
-
-            elif model_name == "arima":
-
-                model = ARIMAModel(order=(1, 1, 1))
-                model.train(consumption_clean)
-
-                n = len(consumption_clean)
-                forecast_horizon = len(test_set)  # or any desired horizon
-                output = model.predict(start=n, end=n + forecast_horizon - 1)
-
-                #if the output contains NaN values, fill them with the mean of the training set
-                if np.isnan(output).any():
-                    print("NaN values found in the output. Filling with mean.")
-                    output = output.fillna(consumption_clean.mean())
-
-            elif model_name == "gaussian_process":
-                # Gaussian Process Model
-                model = GaussianProcessModel()
-                model.train(feature_past, consumption_clean)
-                output = model.predict(feature_future)
-
-            elif model_name == "xgboost":
-
-                model = XGBoostModel()
-                model.train(feature_past, consumption_clean)
-                output = model.predict(feature_future)
-
-            elif model_name == "gam":
-                # Generalized Additive Model (GAM)
-                model = GAMModel()
-                model.train(feature_past, consumption_clean)
-                output = model.predict(feature_future)
-
-            elif model_name == "catboost":
-                # CatBoost Model
-                try:
-                    model = CatBoostModel()
-                    model.train(feature_past, consumption_clean)
-                    output = model.predict(feature_future)
-                except Exception as e:
-                    custom_col = [col for col in feature_past.columns if costumer in col]
-                    output = feature_future[custom_col]
-
-            elif model_name == "ligthgbm":
-                # LightGBM Model
-                model = LightGBMModel()
-                model.train(feature_past, consumption_clean)
-                output = model.predict(feature_future)
-            
-            elif model_name == "xgboost_gridsearch":
-        
-                # Uncomment the grid search code to use GridSearchCV for hyperparameter tuning
-
-
-                # Define the parameter grid
-                param_grid = {
-                    'n_estimators': [ 50, 100, 200],
-                    'max_depth': [3, 5, 7],
-                    'learning_rate': [0.00,0.01, 0.1, 0.2],
-                    'lambda': [0.0, 0.1, 1.0],
-                    'alpha': [0.0, 0.1, 1.0],
-                    'subsample': [0.5, 0.75, 1.0],
-
-                }
-                tss = TimeSeriesSplit(n_splits=3, max_train_size=None, test_size=None, gap=0)
-
-
-                
-                # Initialize the estimator
-                xgb_estimator = XGBRegressor(objective='reg:squarederror', random_state=42)
-
-                ps = PredefinedSplit(test_fold = consumption_clean)
-                
-                # Set up the grid search with 3-fold CV
-                grid_search = GridSearchCV(
-                    estimator=xgb_estimator,
-                    param_grid=param_grid,
-                    cv=tss,
-                    scoring='neg_mean_absolute_error',
-                    verbose=1
-                )
-                
-                # Perform grid search
-                grid_search.fit(feature_past, consumption_clean)
-                
-                # Output the best parameters
-                print("Best parameters found: ", grid_search.best_params_)
-                
-                # Retrieve the best estimator and predict
-                best_model = grid_search.best_estimator_
-                output = best_model.predict(feature_future)
-
-            elif model_name == "multi_time_series":
-                # Multi Time Series Model
-                model = MultiTimeSeriesForecaster()
-                model.train(consumption_clean, feature_past)
-                output = model.predict(feature_future)
-
-            else:
-            
-                print("NO Model specified sofar")
-                raise ValueError("No model specified")
-            
-   
-        
-            forecast[costumer] = output
+        if model_name == "ligthgbm":
+            # LightGBM Model
+            model = LightGBMModel()
+            model.train(feature_past, consumption_clean)
+            output = model.predict(feature_future)
+        else:
+            raise ValueError("No model specified")
+                    
+        forecast[costumer] = output
 
     """
     END OF THE MODIFIABLE PART.
@@ -275,17 +134,12 @@ def main(zone: str, encoding_name: str, model_name: str, train_test: bool, split
                 test_end = range_forecast[-1],
                 logging_folder=logging_folder,
                 feature_sets = feature_sets,
-                global_model = global_model,
                 encoding = encoding_name,
                 )
 
 
 
-    forecast.to_csv(
-        join(output_path, "students_results_" + team_name + "_" + country + ".csv")
-    )
-
-    print(f'Dummy Error {dummy_error}')
+    forecast.to_csv(join(output_path, "students_results_" + team_name + "_" + country + ".csv"))
 
 if __name__ == "__main__":
     country = "IT"  # it can be ES or IT
@@ -310,7 +164,5 @@ if __name__ == "__main__":
             train_test=train_test,
             split_date=split_date,
             feature_sets=features,
-            global_model=global_model,
-            inputation_method=inputation_method,
             start_date=start_date
             )
